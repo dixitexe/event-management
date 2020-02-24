03dc4756ce2ac4353ab8c13973ad1adf3be43453
dixitexe <61399124+dixitexe@users.noreply.github.com>
2020-02-24 08:50:13 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'python8',
-        'USER': 'root',
-        'PASSWORD': 'tops',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3310',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
